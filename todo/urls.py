0053fb3e7e62c8b379487cefa82ea09267862c10
AnujKarmacharya <karmacharyaanuj27@gmail.com>
2024-07-07 10:51:34 +0545

    test

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,14 @@
-
 from django.contrib import admin
-from django.urls import path ,include
 
-from todo.views import index
+from django.urls import path, include
+
+from todo.views import index, contact, create, mark_as_done, delete_task, edit
 
 urlpatterns = [
-    path("", index)
-]
+    path("", index), 
+    path("contact", contact),
+    path("create", create),
+    path("task/<pk>/mark", mark_as_done),
+    path("task/<pk>/delete", delete_task),
+    path("task/<pk>/edit", edit)
+    ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Tasks
 
 # Create your views here.
@@ -11,3 +11,40 @@ def index(request):
     context = {"tasks": tasks}
 
     return render(request, "index.html", context)
+
+
+def contact(request):
+    return render(request, "contact.html")
+
+
+def create(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        description = request.POST.get("description")
+        Tasks.objects.create(name=name, description=description)
+        return redirect("/")
+    return render(request, "create.html")
+
+
+def mark_as_done(request, pk):
+    task = Tasks.objects.get(pk=pk)
+    task.status = True
+    task.save()
+    return redirect("/")
+
+
+def delete_task(request, pk):
+    task = Tasks.objects.get(pk=pk)
+    task.delete()
+    return redirect("/")
+
+
+def edit(request, pk):
+    task = Tasks.objects.get(pk=pk)
+    if request.method == "POST":
+        task.name = request.POST.get("name")
+        task.description = request.POST.get("description")
+        task.save()
+        return redirect("/") 
+    context = {"task": task}
+    return render(request, "edit.html", context)
